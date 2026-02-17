8a165186e8b901d31c7b98b62513248eaa19d5a1
VishalKammari <vishalkodiganti@gmail.com>
2026-02-17 15:00:03 +0530

    added quill backend

--- index.js
+++ index.js
@@ -15,16 +15,22 @@ const allowedOrigins = [
   "https://quill-bp03sa5ys-visha9764s-projects.vercel.app"
 ];
 
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true);
+
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       return callback(new Error("CORS not allowed"), false);
+//     }
+
+//     return callback(null, true);
+//   },
+//   credentials: true
+// }));
+
+
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error("CORS not allowed"), false);
-    }
-
-    return callback(null, true);
-  },
+  origin: true,
   credentials: true
 }));
 
