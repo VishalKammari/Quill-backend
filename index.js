d9e28b86b69d4acb7fca9e2c06ee8c3006efc0d3
VishalKammari <vishalkodiganti@gmail.com>
2026-02-17 15:44:58 +0530

    fixed bugs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const app = express();
+app.set("trust proxy", 1);
 const cors = require('cors');
 
 const multer = require('multer');
