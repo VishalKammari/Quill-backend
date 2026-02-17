ac09736631a50f41f5e2f664249edb0e5dfaeb4f
VishalKammari <vishalkodiganti@gmail.com>
2026-02-17 15:26:55 +0530

    fixed bugs

--- routes/auth.js
+++ routes/auth.js
@@ -44,7 +44,7 @@ router.post('/login', async (req, res) => {
 
     res.cookie("token", token, {
     httpOnly: true,
-    secure: false,     
+    secure: true,     
     sameSite: "None"
   })
   .status(200)
